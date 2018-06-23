7eda41f5591f377c6f20cb7e4b91d5eb783728c3
98max1 <xa-fc@live.com.pt>
2018-06-23 22:17:24 +0100

    class produto

--- app/src/main/java/com/example/a98max1/milpaus/modelo/Produto.java
+++ app/src/main/java/com/example/a98max1/milpaus/modelo/Produto.java
@@ -1,6 +1,13 @@
 package com.example.a98max1.milpaus.modelo;
 
 public class Produto {
+    private String nome;
 
+    public Produto(String nome) {
+        this.nome = nome;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 }
